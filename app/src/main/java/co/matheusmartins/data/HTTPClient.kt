2c42ebe604b29c390f41e0acdef202adb62cb99e
Matheus Martins <matheus_11martins@hotmail.com>
2022-11-20 05:55:45 -0300

    config

--- app/src/main/java/co/matheusmartins/data/HTTPClient.kt
+++ app/src/main/java/co/matheusmartins/data/HTTPClient.kt
@@ -7,6 +7,11 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object HTTPClient {
 
+    /*
+    URL do ChuckNorris = https://api.chucknorris.io/jokes/
+    API do chuck norris está com falha na url de imagem, usarei uma do tiago que é a mesma, porém
+    a imagem está "ok"
+    */
     private const val BASE_URL = "https://api.tiagoaguiar.co/jokerapp/"
     const val API_KEY = "48b5af64-23c4-4480-87fb-26c4a5fba354"
 
